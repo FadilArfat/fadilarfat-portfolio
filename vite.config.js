7311e9f5c5fc95b77abeabd279ddb7bf6b047790
FadilArfat <fadilitas14@gmail.com>
2025-05-21 11:56:37 +0700

    fx

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/fadilarfat-portfolio",
+  base: "/fadilarfat-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
